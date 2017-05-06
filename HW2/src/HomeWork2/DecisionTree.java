f1f321e82573fa638ed8ed5a2ef32b83e716ce34
Barak2222 <barakcohen54@gmail.com>
2017-05-06 14:13:11 +0300

    choose best attribute

--- HW2/src/HomeWork2/DecisionTree.java
+++ HW2/src/HomeWork2/DecisionTree.java
@@ -41,7 +41,7 @@ public class DecisionTree implements Classifier {
    	
 	@Override
 	public void buildClassifier(Instances instances) throws Exception {
-		classIndex = 10;
+		classIndex = 9;
 		
 		// TODO
 		
@@ -69,11 +69,24 @@ public class DecisionTree implements Classifier {
 //			}
 			
 			// if perfectly calssified
-			if(! nodeToProcess.isPerfectlyClassified){
+			if(nodeToProcess.isPerfectlyClassified){
 				continue;
 			}
+			Instances instancesThatGoToNode = instances;// TODO change this
 			
 			// Choose best attribute
+			int bestAttributeIndex = -1;
+			double bestInfoGain = -1;
+			for(int i = 0; i < instances.numAttributes() - 1; i++){
+				double currentInfoGain = calcInfoGain(instancesThatGoToNode, i);
+				if(currentInfoGain > bestAttributeIndex){
+					bestInfoGain = currentInfoGain;
+					bestAttributeIndex = i;
+				}
+			}
+			
+			// create children
+			
 			
 		}
 		// TODO Auto-generated method stub
@@ -82,45 +95,32 @@ public class DecisionTree implements Classifier {
 	
 	private double calcInfoGain(Instances subsetOfTrainingData, int attributeIndex){
 		final int S_SIZE = subsetOfTrainingData.size();
-		double[] numberOfInstancesForClass = new double[2];
 		
 		// First calculate entropy for S (= the subset)
-		for (Instance instance : subsetOfTrainingData) {
-			numberOfInstancesForClass[(int) instance.value(classIndex)] ++;
-		}
+		double entropyForS = calcEntropyForInstances(subsetOfTrainingData);
+		double sumOfChildEntropies = 0;
 		
-		for (int i = 0; i < 2; i++) {
-			numberOfInstancesForClass[i] /= S_SIZE;
-		}
-		double entropyForS = calcEntropy(numberOfInstancesForClass);
-
-		double SumOfSmallEntropies = 0;
+		// eg number of children
 		final int NUMBER_OF_VALUES_FOR_ATTRIBUTE = subsetOfTrainingData.attribute(attributeIndex).numValues();
-
+		double[] childrenProbabilities = new double[NUMBER_OF_VALUES_FOR_ATTRIBUTE];
+		
+		// Iterate children and sum into sumOfChildEntropies
 		for(int attributevalue = 0; attributevalue < NUMBER_OF_VALUES_FOR_ATTRIBUTE; attributevalue++){
-			Instances instancesToProcess = filterInstancesWithAttributeValue(subsetOfTrainingData, attributeIndex, attributevalue);
-			double entropyForCurrent = 0;
+			Instances instancesWithValue = filterInstancesWithAttributeValue(subsetOfTrainingData, attributeIndex, attributevalue);
+			double entropyForCurrent = calcEntropyForInstances(instancesWithValue);
+			sumOfChildEntropies += entropyForCurrent * instancesWithValue.size() / S_SIZE;
 			
-			double[] numberOfInstancesForClass = new double[2];
-			
-			// First calculate entropy for S (= the subset)
-			for (Instance instance : subsetOfTrainingData) {
-				numberOfInstancesForClass[(int) instance.value(classIndex)] ++;
-			}
+			// Aggregate data for splitInformation
+			childrenProbabilities[attributevalue] = instancesWithValue.size() / S_SIZE;
 		}
+		System.out.println();
 		
-		int numOfInstancesInFirstCalss = 0;
-		int numOfInstancesInSecondCalss = 0;
+		// Calculate informationGain
+		double informationGain = entropyForS - sumOfChildEntropies;
 		
-		// Iterate relevant instances
-		for (Instance instance : subsetOfTrainingData) {
-			instance.attribute(attributeIndex);
-			
-			
-		}
-		double[] probablilities = {numOfInstancesInFirstCalss / S_SIZE, numOfInstancesInSecondCalss / S_SIZE};
-		
-		return -1;
+		// Calculate "splitInformation"
+		double splitInformation = calcEntropy(childrenProbabilities);
+		return informationGain / splitInformation;
 	}
 	
 	private double calcEntropyForInstances(Instances instances){
@@ -134,21 +134,33 @@ public class DecisionTree implements Classifier {
 		return calcEntropy(numberOfInstancesForClass);
 	}
 	
-	private static Instances filterInstancesWithAttributeValue(Instances instances, int attributeIndex, double attributevalue){
-		try {
-			RemoveWithValues filter = new RemoveWithValues();
-			String[] options = new String[4];
-			options[0] = "-C";   // Choose attribute to be used for selection
-			options[1] = Integer.toString(attributeIndex); // Attribute number    
-			options[2] = "-L";
-			options[3] = Integer.toString((int) attributevalue);
-			filter.setOptions(options);
-			filter.setInputFormat(instances);
-			return Filter.useFilter(instances, filter);
-		} catch (Exception e) {
-			e.printStackTrace();
+//	private static Instances fildterInstancesWithAttributeValue(Instances instances, int attributeIndex, double attributevalue){
+//		try {
+//			RemoveWithValues filter = new RemoveWithValues();
+//			String[] options = new String[4];
+//			options[0] = "-C";   // Choose attribute to be used for selection
+//			options[1] = Integer.toString(attributeIndex); // Attribute number    
+//			options[2] = "-L";
+//			options[3] = Integer.toString((int) attributevalue);
+//			filter.setOptions(options);
+//			filter.setInputFormat(instances);
+//			return Filter.useFilter(instances, filter);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		throw new IllegalArgumentException();
+//	}
+//	
+	private static Instances filterInstancesWithAttributeValue(Instances source, int attributeIndex, double attributevalue){
+		Instances clone = new Instances(source);
+		for (int i = source.size() - 1; i >= 0; i--) {
+			Instance instance = source.get(i);
+			double attrValueForInstance = instance.value(attributeIndex);
+			if( attrValueForInstance != attributevalue){
+				clone.delete(i);
+			}
 		}
-		throw new IllegalArgumentException();
+		return clone;
 	}
 	
 	private double calcEntropy(double[] probabilities){
