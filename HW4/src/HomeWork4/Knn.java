88fdd47a2d0ba081ee6ac89910922ded1ba855f0
Barak2222 <barakcohen54@gmail.com>
2017-05-31 14:24:43 +0300

    implamentations

--- HW4/src/HomeWork4/Knn.java
+++ HW4/src/HomeWork4/Knn.java
@@ -2,11 +2,15 @@ package HomeWork4;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Random;
 
-import HomeWork4.Knn.HyperParameters.PDistanceOptions;
+import HomeWork4.Knn.HyperParameters.Majority;
+import HomeWork4.Knn.HyperParameters.LPDistanceOptions;
 import weka.classifiers.Classifier;
 import weka.core.Capabilities;
 import weka.core.Instance;
@@ -17,6 +21,7 @@ public class Knn implements Classifier {
 	public enum EditMode {None, Forwards, Backwards};
 	private EditMode m_editMode = EditMode.None;
 	private Instances m_trainingInstances;
+	private int numberOfClasses;
 	public HyperParameters hyperParameters;
 
 	public EditMode getEditMode() {
@@ -29,6 +34,7 @@ public class Knn implements Classifier {
 	
 	public HyperParameters findBestHyperParameters(Instances instances){
 		shuffleInstances(instances);
+		numberOfClasses = instances.attribute(instances.classIndex()).numValues();
 		List<HyperParameters> options  = HyperParameters.getHyperParametersPermutations();
 		double bestError = Double.MAX_VALUE;
 		HyperParameters bestHyperParameters = null;
@@ -50,7 +56,7 @@ public class Knn implements Classifier {
 	@Override
 	public void buildClassifier(Instances arg0) throws Exception {
 		shuffleInstances(arg0);
-		
+		numberOfClasses = arg0.attribute(arg0.classIndex()).numValues();
 		switch (m_editMode) {
 		case None:
 			noEdit(arg0);
@@ -74,8 +80,11 @@ public class Knn implements Classifier {
 	 */
 	@Override
 	public double classifyInstance(Instance instance) {
-		return 0;
-		//TODO: implement this method
+		Map<Instance, Double> neighbors = findNearestNeighbors(instance);
+		if(hyperParameters.majority == Majority.weighted){
+			return getWeightedClassVoteResult(neighbors);
+		}
+		return getClassVoteResult(neighbors);
 	}
 	
 	/**
@@ -95,15 +104,21 @@ public class Knn implements Classifier {
 	}
 	
 	/**
-	 * : Calculate the average error on a given instances set. The average error is the total number of classification mistakes on the input 
+	 * Calculate the average error on a given instances set. The average error is the total number of classification mistakes on the input 
 	 * instances set and divides that by the number of instances in the input set.
 	 * @param instances instances
 	 * @return classification error
 	 */
 	private double calcAvgError(Instances instances){
-		// TODO
-		return 0;
-		
+		int numOfErrors = 0;
+		for (Instance instance : instances) {
+			double predictedClass = classifyInstance(instance);
+			double actualClass = instance.value(instance.classIndex());
+			if(predictedClass != actualClass){
+				numOfErrors++;
+			}
+		}
+		return ((double) numOfErrors) / instances.size(); 
 	}
 	
 	/**
@@ -123,18 +138,59 @@ public class Knn implements Classifier {
 	 * @return Average fold error (double)
 	 */
 	private double crossValidationError(Instances instances, int numOfFolds) {
-		// TODO
-		return 0;
+		double sumOfErrors = 0;
+		for(int foldNumber = 0; foldNumber < numOfFolds; foldNumber++){
+			
+			// Split to training and validation
+			Instances training = new Instances(instances);
+			Instances validation = new Instances(instances);
+			training.clear();
+			validation.clear();
+			for(int i = 0; i < instances.size(); i++){
+				if(i / numOfFolds == foldNumber){
+					validation.add(instances.get(i));
+				} else {
+					training.add(instances.get(i));
+				}
+			}
+			
+			// Calc error
+			this.m_trainingInstances = training;
+			double error = calcAvgError(validation);
+			sumOfErrors+= error;
+			
+		}
+		
+		return sumOfErrors / numOfFolds;
 	}
 	
 	/**
-	 * the K nearest neighbors for the instance being classified.
-	 * @param instance input instance
-	 * @return K nearest neighbors and their distance from the given instance
+	 * Find the K nearest neighbors for the instance being classified(target).
+	 * @param target input instance
+	 * @return k nearest neighbors and their distance from the given instance
 	 */
-	private Map<Instance, Double> findNearestNeighbors(Instance instance){
-		// TODO
-		return null;
+	private Map<Instance, Double> findNearestNeighbors(Instance target){
+		Map<Instance, Double> result = new HashMap<>();
+		Comparator<Instance> distanceComparator = new Comparator<Instance>() {
+			@Override
+			public int compare(Instance o1, Instance o2) {
+				double d1 = distance(target, o1);
+				double d2 = distance(target, o2);
+				return Double.compare(d1, d2) * (-1);
+			}
+		};
+		
+		// Initialize priority queue of size K with the relevant comparision function
+		PriorityQueue<Instance> queue = new PriorityQueue<>(hyperParameters.k, distanceComparator);
+		for (Instance neighbor : m_trainingInstances) {
+			queue.add(neighbor);
+		}
+		
+		// Process data
+		for (Instance closestNeighbor : queue) {
+			result.put(closestNeighbor, distance(closestNeighbor, target));
+		}
+		return result;
 	}
 	
 	/**
@@ -142,9 +198,24 @@ public class Knn implements Classifier {
 	 * @param nearestNeighbors The k nearest neighbors
 	 * @return the majority vote on the class of the neighbors
 	 */
-	private double getClassVoteResult(Map<Instance, Double> nearestNeighbors){
-		// TODO
-		return 0;
+	private double getClassVoteResult(Map<Instance, Double> nearestNeighborsWithDistances){
+		if(nearestNeighborsWithDistances.isEmpty()){
+			throw new IllegalArgumentException();
+		}
+		int[] countersArr = new int[numberOfClasses];
+		for (Instance neighbor : nearestNeighborsWithDistances.keySet()) {
+			countersArr[(int) neighbor.value(neighbor.classIndex())]++;
+		}
+		
+		double bestClass = -1.0;
+		int highestVotes = -1;
+		for (int i = 0; i < countersArr.length; i++) {
+			if(countersArr[i] > highestVotes){
+				highestVotes = countersArr[i];
+				bestClass = (double) i;
+			}
+		}
+		return bestClass;
 	}
 	
 	/**
@@ -155,9 +226,25 @@ public class Knn implements Classifier {
 	 * @return the majority vote on the class of the neighbors, where each neighbor's class is weighted by the neighbor’s
 	 * distance from the instance being classified.
 	 */
-	private double getWeightedClassVoteResult(Map<Instance, Double> nearestNeighbors){
-		// TODO
-		return 0;
+	private double getWeightedClassVoteResult(Map<Instance, Double> nearestNeighborsWithDistances){
+		if(nearestNeighborsWithDistances.isEmpty()){
+			throw new IllegalArgumentException();
+		}
+		double[] weightsArr = new double[numberOfClasses];
+		for (Instance neighbor : nearestNeighborsWithDistances.keySet()) {
+			int classIndex = (int) neighbor.value(neighbor.classIndex());
+			weightsArr[classIndex]+= (double) 1/(Math.pow(nearestNeighborsWithDistances.get(neighbor), 2));
+		}
+		
+		double bestClass = -1.0;
+		double highestweight = -1;
+		for (int i = 0; i < weightsArr.length; i++) {
+			if(weightsArr[i] > highestweight){
+				highestweight = weightsArr[i];
+				bestClass = (double) i;
+			}
+		}
+		return bestClass;
 	}
 	
 	/**
@@ -167,8 +254,13 @@ public class Knn implements Classifier {
 	 * @return the input instances’ distance according to the distance function that your algorithm is configured to use.
 	 */
 	private double distance(Instance a, Instance b){
-		// TODO => should use the fillowing two methods: OnePDistance, OneInfinityDistance
-		return 0;
+		switch(hyperParameters.lpDistance){
+			case one:      { return OnePDistance(a, b, 1); }
+			case two:      { return OnePDistance(a, b, 2); }
+			case three:    { return OnePDistance(a, b, 3); }
+			case infinity: { return OneInfinityDistance(a, b);}
+			default:       { throw new IllegalArgumentException();}
+		}
 	}
 	
 	/**
@@ -178,9 +270,14 @@ public class Knn implements Classifier {
 	 * @param pDistance p parameter to use
 	 * @return the l-p distance between the two instances
 	 */
-	private double OnePDistance(Instance a, Instance b, PDistanceOptions pDistance){
-		// TODO
-		return 0;
+	private double OnePDistance(Instance a, Instance b, int pDistance){
+		double sum = 0;
+		for(int attr = 0; attr < a.numAttributes() - 1; attr++){
+//			sum+= TODO
+		}
+		
+		
+		return Math.pow(sum, (1/pDistance));
 	}
 	
 	/**
@@ -220,20 +317,20 @@ public class Knn implements Classifier {
 	
 	public static class HyperParameters {
 		public enum Majority { uniform, weighted;}
-		public enum PDistanceOptions { one, two, three, infinity }
+		public enum LPDistanceOptions { one, two, three, infinity }
 		
 		public int k;
-		public PDistanceOptions pDistance;
+		public LPDistanceOptions lpDistance;
 		public Majority majority;
 		
 		public static List<HyperParameters> getHyperParametersPermutations (){
 			List<HyperParameters> result = new ArrayList<>();
 			for(Majority majority: Majority.values()){
-				for(PDistanceOptions pDistance: PDistanceOptions.values()){
+				for(LPDistanceOptions pDistance: LPDistanceOptions.values()){
 					for(int k = 1; k <= 20; k++){
 						HyperParameters hp = new HyperParameters();
 						hp.k = k;
-						hp.pDistance = pDistance;
+						hp.lpDistance = pDistance;
 						hp.majority = majority;
 						result.add(hp);
 					}
@@ -243,7 +340,17 @@ public class Knn implements Classifier {
 		}
 		
 		public String toString(){
-			return MessageFormat.format("k: {0}, 1-p Distance: {1}, Majority: {2}", this.k, this.pDistance, this.k);
+			return MessageFormat.format("k: {0}, 1-p Distance: {1}, Majority: {2}", this.k, this.lpDistance, this.k);
 		}
 	}
+	
+//	static class InstancesPriorityQueue{
+//		private PriorityQueue queue;
+//		private Instance target;
+//		public InstancesPriorityQueue(Instance target, int size){
+//			this.target = target;
+//			queue = new PriorityQueue<>(size, new Comparator<Instance>() {
+//			})
+//		}
+//	}
 }
