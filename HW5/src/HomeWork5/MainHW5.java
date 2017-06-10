0a3e5f9761806222d0445f43364bd52e185d4142
Arik Muller <arik@myfanzone.com>
2017-06-10 14:47:54 +0300

    Started implementing main function

--- HW5/src/HomeWork5/MainHW5.java
+++ HW5/src/HomeWork5/MainHW5.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.Random;
 
 import weka.core.Instances;
@@ -33,5 +34,55 @@ public class MainHW5 {
 	}
 	
 	public static void main(String[] args) throws Exception {
+		Instances cancerInstances = loadData("cancer.txt");
+		SVM svm;
+		int polynomialKernelValues[] = { 2, 3, 4 };
+		double RBFKernelValues[] = { 0.01, 0.1, 1 };
+		int TPR = 1;
+		int FPR = 1; 
+		
+		// TODO:
+		// Divide the data to training and test set - 80% training and 20% test
+		Instances trainingSet = null;
+		Instances testSet = null;
+		
+		// For each kernel value, build the SVM classifier on the training set using the SMO WEKA class
+		for (int i = 0; i < polynomialKernelValues.length; i++) {
+			for (int j = 0; j < RBFKernelValues.length; j++) {
+				svm = new SVM();
+				
+				// Set kernel to be polynomial
+				PolyKernel polyKernel = new PolyKernel();
+				polyKernel.setExponent(i);
+				svm.setKernel(polyKernel);
+				svm.buildClassifier(trainingSet);
+				
+				// TODO: Calculate TPR and FPR for poly
+				TPR = 0;
+				FPR = 0;
+				
+				System.out.println(
+						MessageFormat.format("For PolyKernel with degree {0} the rates are:\nTPR = {1}\nFPR = {2}", i, TPR, FPR));
+				
+				// Set kernel to be RBF
+				RBFKernel rbfKernel = new RBFKernel();
+				rbfKernel.setGamma(j);
+				svm.setKernel(rbfKernel);
+				svm.buildClassifier(trainingSet);
+				
+				// TODO: Calculate TPR and FPR for poly
+				TPR = 0;
+				FPR = 0;
+				
+				System.out.println(
+						MessageFormat.format("For RBFKernel with gamma {0} the rates are:\nTPR = {1}\nFPR = {2}", j, TPR, FPR));
+
+			}
+		}
+		// TODO: Check what's the best kernel (poly or RBF), and save the best TPR & FPR
+		System.out.println(
+				MessageFormat.format("The best kernel is: <Poly or RBF> <kernel parameter (degree or gamma)> <TPR - FPR>"));
+		
+		
 	}
 }
